d16c5d4e05a12a89440f4f6ec025b9c06ac3dcb9
asdfqaz74 <74591618+asdfqaz74@users.noreply.github.com>
2024-07-09 18:14:36 +0900

    [feat] global navigation css

--- src/components/globalNav.jsx
+++ src/components/globalNav.jsx
@@ -1,13 +1,33 @@
+import Image from "next/image";
 import Link from "next/link";
 
 export default function GlobalNav() {
   return (
-    <nav>
-      <ul>
+    <nav className="bg-light rounded shadow-lg justify-center items-center flex h-[7rem]">
+      <ul className="font-bold text-lg flex items-center gap-x-36 px-4">
         <li>
-          <Link href="/">HOME</Link>
+          <Link href="/">
+            <Image
+              src="/logo2.png"
+              width={200}
+              height={117}
+              alt=""
+              className="object-cover"
+            />
+          </Link>
+        </li>
+        <li className="hover:text-primaryHover link-underline link-underline-black">
+          <Link className="" href="/">
+            HOME
+          </Link>
+        </li>
+        <li className="hover:text-primaryHover link-underline link-underline-black">
           <Link href="/talents">TALENTS</Link>
+        </li>
+        <li className="hover:text-primaryHover link-underline link-underline-black">
           <Link href="/music">MUSIC</Link>
+        </li>
+        <li className="hover:text-primaryHover link-underline link-underline-black">
           <Link href="/contact">CONTACT</Link>
         </li>
       </ul>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,26 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {},
+      backgroundImage: {
+        "link-underline":
+          "linear-gradient(transparent, transparent), linear-gradient(#fff, #fff)",
+        "link-underline-black":
+          "linear-gradient(transparent, transparent), linear-gradient(#8190e6, #8190e6)",
+      },
+      backgroundSize: {
+        "size-0-3": "0 3px",
+        "size-100-3": "100% 3px",
+      },
+      backgroundPosition: {
+        "pos-0-100": "0 100%",
+        "pos-100-100": "100% 100%",
+      },
+      transitionTimingFunction: {
+        "ease-in-out": "ease-in-out",
+      },
+      transitionProperty: {
+        size: "background-size",
+      },
       fontFamily: {
         sans: ["Montserrat", "sans-serif"],
       },
@@ -17,6 +36,18 @@ module.exports = {
         medium: 500,
         semibold: 600,
         bold: 700,
+      },
+      colors: {
+        light: "#f4f6ff",
+        lightHover: "#eef1ff",
+        lightActive: "#dce2ff",
+        primary: "#8fa0ff",
+        primaryHover: "#8190e6",
+        primaryActive: "#7280cc",
+        dark: "#6b78bf",
+        darkHover: "#566099",
+        darkActive: "#404873",
+        darker: "#323859",
       },
     },
   },
